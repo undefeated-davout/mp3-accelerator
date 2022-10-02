914aaa57c02619304a0dd1585c0cc044b9e1e852
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-10-02 22:37:46 +0900

    add: mp3変換スクリプト

---
+++ app/mp3-accelerator.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8
+
+import argparse
+import glob
+import os
+import subprocess
+
+input_dir = "./data/input/"
+output_dir = "./data/output/"
+
+
+def main(speed):
+    file_paths = glob.glob(input_dir + "*.mp3")
+    for file_path in file_paths:
+        print(file_path)
+        file_name = os.path.basename(file_path)
+        output_path = output_dir + file_name
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-i",
+                file_path,
+                "-af",
+                "atempo=" + str(speed),
+                output_path,
+            ]
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="mp3の速度を変更する")
+    parser.add_argument(
+        "speed",
+        type=float,
+        help="変換倍率",
+    )
+    args = parser.parse_args()
+    print("倍率: " + str(args.speed))
+
+    main(args.speed)
