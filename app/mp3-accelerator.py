bc1e5fe7735ffb039a7501a50bfb683fc2ba151f
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-10-05 13:29:48 +0900

    modify: 並列処理できるよう修正

--- app/mp3-accelerator.py
+++ app/mp3-accelerator.py
@@ -4,30 +4,36 @@ import argparse
 import glob
 import os
 import subprocess
+from concurrent.futures import ProcessPoolExecutor
 
-input_dir = "./data/input/"
-output_dir = "./data/output/"
+INPUT_DIR = "./data/input/"
+OUTPUT_DIR = "./data/output/"
+MAX_WORKERS = 4
+
+
+def conv_mp3(file_path, speed):
+    file_name = os.path.basename(file_path)
+    output_path = OUTPUT_DIR + file_name
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            file_path,
+            "-af",
+            "atempo=" + str(speed),
+            output_path,
+        ]
+    )
 
 
 def main(speed):
-    file_paths = glob.glob(input_dir + "*.mp3")
-    print("ファイル数: " + str(len(file_paths)))
-    count = 1
-    for file_path in file_paths:
-        print(str(count) + "個目処理中: " + file_path)
-        file_name = os.path.basename(file_path)
-        output_path = output_dir + file_name
-        subprocess.run(
-            [
-                "ffmpeg",
-                "-i",
-                file_path,
-                "-af",
-                "atempo=" + str(speed),
-                output_path,
-            ]
-        )
-        count += 1
+    file_paths = glob.glob(INPUT_DIR + "*.mp3")
+    file_count = len(file_paths)
+    print("ファイル数: " + str(file_count))
+
+    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        executor.map(conv_mp3, file_paths, [speed] * file_count)
+        print("タスクセット完了")
 
 
 if __name__ == "__main__":
